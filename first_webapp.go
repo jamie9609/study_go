57935a373e29463310ed73933bc233ebf4e71a25
jamie9609 <714468438@qq.com>
2019-11-14 00:24:46 +0800

    first

--- first_webapp.go
+++ first_webapp.go
@@ -1,4 +1,15 @@
 package main
 
-import
+import (
+	"fmt"
+	"net/http"
+)
 
+func handler(write http.ResponseWriter, request *http.Request)  {
+	fmt.Fprintf(writer, "hello world, %s!", request.URL.Path[1:])
+}
+
+func main()  {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
