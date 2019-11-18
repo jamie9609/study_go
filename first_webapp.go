63d2a3e1b0cf43ee02ebb263d035202222026e7e
jamie9609 <714468438@qq.com>
2019-11-18 10:29:00 +0800

    1117-1118study

--- first_webapp.go
+++ first_webapp.go
@@ -5,11 +5,19 @@ import (
 	"net/http"
 )
 
-func handler(writer http.ResponseWriter, request *http.Request)  {
-	fmt.Fprintf(writer, "hello world, %s!", request.URL.Path[1:])
+type MyHandler struct{}
+
+
+func (h *MyHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request)  {
+	fmt.Fprintf(writer, "hello world1, %s!", request.URL.Path[1:])
 }
 
 func main()  {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	handler := &MyHandler{}
+	server := http.Server{
+		Addr:"127.0.0.1:8080",
+		Handler: handler,
+	}
+	server.ListenAndServe()
 }
+
